c2bf614d0cbeefd847262f021e0451b505776fac
Hipitsu <128088965+Hipitsu@users.noreply.github.com>
2024-02-05 14:26:20 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Ukonsaari',
+        firstname: 'Hilla'
+    },
+    {
+        id: '2',
+        lastname: 'Mäkipelto',
+        firstname: 'Olli'
+    },
+    {
+        id: '3',
+        lastname: 'Kallio',
+        firstname: 'Matti'
+    },
+    {
+        id: '4',
+        lastname: 'Mattila',
+        firstname: 'Heini'
+    },
+    {
+        id: '5',
+        lastname: 'Vuorela',
+        firstname: 'Tuula'
+    },
+    {
+        id: '6',
+        lastname: 'Aho',
+        firstname: 'Niko'
+    },
+    {
+        id: '7',
+        lastname: 'Aura',
+        firstname: 'Jaana'
+    },
+    {
+        id: '8',
+        lastname: 'Koskela',
+        firstname: 'Martti'
+    },
+    {
+        id: '9',
+        lastname: 'Eno',
+        firstname: 'Juhani'
+    },
+    {
+        id: '10',
+        lastname: 'Törmi',
+        firstname: 'Oona'
+    },
+    {
+        id: '11',
+        lastname: 'Kuusi',
+        firstname: 'Pauliina'
+    },
+    {
+        id: '12',
+        lastname: 'Rahola',
+        firstname: 'Jonna'
+    },
+    {
+        id: '13',
+        lastname: 'Rihtiniemi',
+        firstname: 'Jaska'
+    },
+    {
+        id: '14',
+        lastname: 'Rautavuori',
+        firstname: 'Jari'
+    },
+    {
+        id: '15',
+        lastname: 'Kairamo',
+        firstname: 'Aino'
+    },
+]
